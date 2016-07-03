72537795847b14add84d75d307226bf76aaa8632
nuved <nuved@users.noreply.github.com>
2016-07-03 16:51:43 +0430

    small fix for rtl languages

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,6 +1,30 @@
 (function($) {
 
   $(document).ready(function() {
+
+$.each($(".wiki, .wiki-edit"), function(i,v){
+    var txt = $(v).text();
+    for(ci in txt) {
+        if (txt.charCodeAt(ci) > 0x600 && txt.charCodeAt(ci) < 0x700) {
+            $(v).css("direction", "rtl");
+            break;
+        }
+    }
+});
+
+$.each($(".issue .subject a"), function(i,v){
+    var txt = $(v).text();
+    console.log(txt);
+    for(ci in txt) {
+        if (txt.charCodeAt(ci) > 0x600 && txt.charCodeAt(ci) < 0x700) {
+            $(v).parent().css("direction", "rtl");
+            $(v).parent().css("text-align", "right");
+            break;
+        }
+    }
+});
+
+
     var update = $('#update')
     var close = $("<div>", {'class': "update-close"}).text('x');
 
